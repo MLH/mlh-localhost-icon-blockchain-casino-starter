1459ef81cf15a2e4d38f7c0075472451d2a0de99
esouthren <eilidhsouthren@gmail.com>
2019-11-13 15:02:41 +0000

    Update config.py

--- webapp/config.py
+++ webapp/config.py
@@ -10,9 +10,9 @@ CASINO_SCORE_ADDRESS = os.getenv(
     "CASINO_SCORE_ADDRESS", "cx0000000000000000000000000000000000000000"
 )
 PLAYER_WALLET_PRIVATE_KEY_FILE_PATH = os.getenv(
-    "PLAYER_WALLET_PRIVATE_KEY_FILE_PATH", "./keystores/test_icx.json"
+    "PLAYER_WALLET_PRIVATE_KEY_FILE_PATH", "./keystores/keystore1.json"
 )
-PLAYER_WALLET_PASSWORD = os.getenv("PLAYER_WALLET_PASSWORD", "Mlh_icon_Bl0ckchain")
+PLAYER_WALLET_PASSWORD = os.getenv("PLAYER_WALLET_PASSWORD", "test1_Account")
 BET_AMOUNT = int(os.getenv("BET_AMOUNT", 1000))
 DEBUG = bool(os.getenv("DEBUG", True))
 HOST = os.getenv("HOST", "0.0.0.0")
